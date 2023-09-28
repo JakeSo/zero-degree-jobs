2fc14bd8d9d643ff18f3738ce13bf57fd7e48a31
Jake Sorrentino <Jksorrentino@gmail.com>
2023-09-28 17:39:20 -0400

    Fixed routes. Adjusted styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { Container } from 'react-bootstrap'; 
 import CustomNavbar from './components/Navbar';
 import Home from './pages/Home';
@@ -7,18 +7,16 @@ import './App.css';
 
 function App() {
  return (
-  <Router>
       <div className="App">
         <CustomNavbar /> {/* Include your Navbar */}
         <Container fluid>
           {/* Set up routes */}
           <Routes>
-            <Route exact path="/" element={<Home />} /> {/* Home page */}
+            <Route path="/" exact element={<Home />} />{/* Home page */}
             {/* <Route path="/job/:id" component={JobDetail} /> Job detail page */}
           </Routes>
         </Container>
       </div>
-    </Router>
   );
 }
 
